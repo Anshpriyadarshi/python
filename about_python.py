ea7c2823cb505073f7f435036cf507abee624914
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-08-21 16:53:00 +0530

    file added

---
+++ about_python.py
@@ -0,0 +1,2 @@
+# python is a high-level interpreated programming language
+print("learning python is a beginer friendly wat to learn coding easily")
---
+++ welcome.py
@@ -0,0 +1,3 @@
+
+# the purpose of the program to clear the basics of python
+print(" welcome to the code")
