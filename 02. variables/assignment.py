81d29dee7986f3ecd8c03a92e40985425552337e
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-08-22 16:55:28 +0530

    file assignment.py added

---
+++ 02. variables/assignment.py
@@ -0,0 +1,39 @@
+#student-information
+name="ansh"
+age="19"
+city="patna"
+print(name,age,city)
+
+# question2
+name="ansh"
+roll_number="112508"
+student_branch="cse with AI/ML"
+print(name,roll_number,student_branch)
+
+# question 3
+marks="88"
+marks="100"
+print(marks)
+
+# question 4
+name,age,city="rahul",18,"patna"
+print(name,age,city)
+
+# question 5
+x=y=z= "0"
+print(x,y,z)
+
+
+# question 6
+student1="Rahul"
+student_name="Rahul"
+cource="B'tech"
+print(student1,student_name,cource)
+
+# question 7
+#student-information
+name="ansh"
+age="18"
+age="19"
+student_cource="B.tech"
+print(name,age,student_cource)
