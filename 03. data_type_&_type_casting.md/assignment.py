c81fec0d7ee49de9e2b23a8f9e3c2863a23d2351
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-08-24 18:08:46 +0530

    file added

---
+++ 03. data_type_&_type_casting.md/assignment.py
@@ -0,0 +1,80 @@
+
+# question 24
+
+student_name="ansh"        # creating variables
+student_age=19
+student_height=5.11
+is_student=True
+result=None
+
+print(type(student_name))        # identify the type 
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(result))
+
+# question 25
+                        
+a=30                     #Create three variables
+b=2.0
+c="10"
+
+print(type(a))                             # identify the type 
+print(type(b))
+print(type(c))
+
+#question 26
+
+a=True                             #Create two variables
+b="True"
+
+print(type(a))                     #identify the type
+print(type(b))
+
+#question 27
+
+a=None                              #Create two variables
+b="None"   
+
+print(type(a))                     #identify the type
+print(type(b))   
+
+# question 28
+
+value=200
+print(type(value))   
+value=str(value)
+print(type(value))                  #here data type changed from int to string
+
+# question 29
+
+product_name="indo"
+product_quantity=20
+product_price=222.2
+is_available=True
+discount=None
+
+print(type(product_name))        # identify the type 
+print(type(product_quantity))
+print(type(product_price))
+print(type(is_available))
+print(type(discount))
+
+#question 30
+
+a=10
+b=10.0
+c="10"
+d=True
+e="true"
+f=None
+g="none"
+
+print(type(a))        # identify the type  #it is int
+print(type(b))          #it is float
+print(type(c))          #it is string
+print(type(d))          #it is bool
+print(type(e))          #it is string
+print(type(f))          #it is none type
+print(type(g))          # it is string
+
