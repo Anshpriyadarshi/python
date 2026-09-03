b7024f1bf09667d3c08a8bfe8516398f9a30e4be
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-03 23:39:15 +0530

    msg

---
+++ 06. boolean_&_logical/Assignment.py
@@ -0,0 +1,22 @@
+# Question 25
+
+name="aa"
+print(50>5) 
+
+name="aa"
+print(10<20)
+
+# Question 26
+
+name="aa"
+print(10>5) 
+
+name="aa"
+print(100<200)
+
+# question 27
+
+age = 20
+print(not age < 18 or age > 28 and age > 2)
+
+
