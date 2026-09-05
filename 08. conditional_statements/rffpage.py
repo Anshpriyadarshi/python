d1975aaa6b84999bbc96f3cfc54e7bb368910ea7
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-05 22:04:46 +0530

    file added

--- 08. conditional_statements/rffpage.py
+++ 08. conditional_statements/rffpage.py
@@ -1,11 +1,39 @@
+# for result 
 
-Result=int(input("Enter result:-"))
-if Result>32 :
-    print("pass")
+# Result=int(input("Enter result:-"))
+# if Result>32 :
+#     print("pass")
 
-if Result<32 :
-    print("fail")
+# if Result<32 :
+#     print("fail")
 
-if Result==32 :
-    print("pass")
+# if Result==32 :
+#     print("pass")
 
+# for vote
+
+# is_indian=input("Are you indian ? yes or no:")
+
+# if is_indian=="yes":
+#     age=int(input("Enter your age:-"))
+#     if age>=18:
+#         print("allowed to vote")
+#     if age<18:
+#         print("you are not allowed to vote")    
+
+# if is_indian=="no":
+#         print("you are not allowed to vote")
+
+# for if else
+
+hacker=input("are you hacker:-")
+
+if hacker=="yes":
+    age=int(input("Enter your ag:-"))
+    if age>=18:
+        print("allowed")
+    else :
+        print("getlost")
+    
+else :
+    print("getout")
