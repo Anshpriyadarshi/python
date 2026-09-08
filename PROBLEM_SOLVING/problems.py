96dde9e27d306626aec7e55e01d463070228a6e5
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-08 23:33:38 +0530

    file added

---
+++ PROBLEM_SOLVING/problems.py
@@ -0,0 +1,83 @@
+# #Question 1
+# num = int(input("Enter the number:~"))
+# if num>0:
+#     print("POSITIVE")
+# if num<0:
+#     print("NEGATIVE")
+# else:
+#     print("ZERO")
+
+# #Question 2
+# num=int(input("Enter the number:~"))
+
+# if num==0:
+#     print("ZERO")
+# elif num==0:
+#     if num % 2==0:
+#         print("POSITIVE  EVEN")
+#     else :
+#         print("POSITIVE  ODD")
+# else:
+#     if num % 2==0:
+#         print("NEGATIVE  EVEN")
+#     else:
+#         print("NEGATIVE  ZERO")
+
+# #Question 3
+# num1=int(input("Enter the number1:~"))
+# num2=int(input("Enter the number2:~"))
+# if num1==num2:
+#     print("Equal")
+# elif num1>num2:
+#     print(num1)
+# else :
+#     print(num2)
+
+# # #question 4
+# num1=int(input("Enter the number1:~"))
+# num2=int(input("Enter the number2:~"))
+# num3=int(input("Enter the number3:~"))
+# if num1==num2==num3:
+#     print("ZERO")
+# elif num1<num2 and num3: 
+#     print(num1)
+# if num2<num2 and num3:
+#     print(num2)
+# elif num3<num2 and num1:
+#     print(num3)
+# if num1<num2==num3:
+#     print(num1)
+# elif num2<num3==num1:
+#     print(num2)
+# if num3<num1==num2:
+#     print(num3)
+
+# #question 5
+# num1=int(input("Enter the number1:~"))
+# num2=int(input("Enter the number2:~"))
+# num3=int(input("Enter the number3:~"))
+# if num1==num2==num3:
+#     print("ZERO")
+# elif num1>num2 and num3: 
+#     print(num1)
+# if num2>num2 and num3:
+#     print(num2)
+# elif num3>num2 and num1:
+#     print(num3)
+# if num1>num2==num3:
+#     print(num1)
+# elif num2>num3==num1:
+#     print(num2)
+# if num3>num1==num2:
+#     print(num3)
+
+#question 6
+num=int(input("Enter the number:~"))
+if num%5==0 and num%11==0:
+    print("Divisible by 5 and 11")
+elif num%5==0:
+    print("Divisible by 5")
+if num%11==0:
+    print("Divisible by 11")
+else:
+    print("none")
