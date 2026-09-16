1e72bd2937fb7cc93bc4a29225b32a97d7bb73d1
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-16 16:07:26 +0530

    origin main

--- 09. problem_solving/rffpage.py
+++ 09. problem_solving/rffpage.py
@@ -1,10 +1,10 @@
-# num=int(input("Enter 3 digit number:~"))
-# sum=num%10
-# num=num//10
-# sum=sum+num%10
-# num=num//10
-# sum=sum+num%10
-# print(sum)
+num=int(input("Enter 3 digit number:~"))
+sum=num%10
+num=num//10
+sum=sum+num%10
+num=num//10
+sum=sum+num%10
+print(sum)
 
 # condition:- set of rules to perform during coding
 # constrant:- limitation of the input 
