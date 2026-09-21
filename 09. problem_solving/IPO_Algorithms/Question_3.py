fac5359fd56cc02a938a6719aa1c04d7208df2c1
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-21 11:46:13 +0530

    file added

---
+++ 09. problem_solving/IPO_Algorithms/Question_3.py
@@ -0,0 +1,51 @@
+# IPO 
+# """input"""
+# number 1
+# number 2
+# number 3
+
+#"""Processing"""
+# if num1==num2==num3:
+#     print("Equal")
+# elif num1>num2 and num3: 
+#     print(num1)
+# if num2>num2 and num3:
+#     print(num2)
+# elif num3>num2 and num1:
+#     print(num3)
+# if num1>num2==num3:
+#     print(num1)
+# elif num2>num3==num1:
+#     print(num2)
+# if num3>num1==num2:
+#     print(num3)
+
+# output 
+# bigger number is printed
+
+#Algorithms
+
+#Start
+#Read NUmbers
+#compairs the number
+#print biggest one 
+#Stop
+
+#code
+num1=int(input("Enter the number1:~"))
+num2=int(input("Enter the number2:~"))
+num3=int(input("Enter the number3:~"))
+if num1==num2==num3:
+    print("Equal")
+elif num1>num2 and num3: 
+    print(num1)
+if num2>num2 and num3:
+    print(num2)
+elif num3>num2 and num1:
+    print(num3)
+if num1>num2==num3:
+    print(num1)
+elif num2>num3==num1:
+    print(num2)
+if num3>num1==num2:
+    print(num3)
