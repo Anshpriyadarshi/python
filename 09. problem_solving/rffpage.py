a6a644ff4ce2b2d1e3cde5d5dc754cece1d8a0b3
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-24 16:54:24 +0530

    file added

--- 09. problem_solving/rffpage.py
+++ 09. problem_solving/rffpage.py
@@ -1,10 +1,10 @@
-# num=int(input("Enter 3 digit number:~"))
-# sum=num%10
-# num=num//10
-# sum=sum+num%10
-# num=num//10
-# sum=sum+num%10
-# print(sum)
+num=int(input("Enter 3 digit number:~"))
+sum=num%10
+num=num//10
+sum=sum+num%10
+num=num//10
+sum=sum+num%10
+print(sum)
 
 # condition:- set of rules to perform during coding
 # constrant:- limitation of the input 
@@ -12,18 +12,18 @@
 
 # testtag - html,css,python ,java script
 
-# for i in range(1,6):
-#     print(" *" * i)
+for i in range(1,6):
+    print(" *" * i)
 
-# number=int(input("Enter the number:~"))
+number=int(input("Enter the number:~"))
 
-# for i in range(1,number+1):
-#     for j in range (1,number+1):
-#         if j==1 or j==number or i==number:
-#             print("* ",end="")
-#         else :
-#             print("  ", end="")
-#     print()
+for i in range(1,number+1):
+    for j in range (1,number+1):
+        if j==1 or j==number or i==number:
+            print("* ",end="")
+        else :
+            print("  ", end="")
+    print()
 
     
 
