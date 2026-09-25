d698f68b1371d721cbe66672b373b00848611cf3
Anshpriyadarshi <Ansh.priyadarshi.cg@gmail.com>
2026-09-25 11:28:42 +0530

    file added

--- 09. problem_solving/Nested_for_Loop_Assignment/Assignment.py
+++ 09. problem_solving/Nested_for_Loop_Assignment/Assignment.py
@@ -9,3 +9,72 @@ for i in range(1,4):
     for j in range(1,4):
         print(j,end=" ")
     print()
+
+#Question 3
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,end="")
+    print()
+
+#Question 4
+for i in range(6):
+    for j in range(1,i+1):
+        print("* ",end="")
+    print()
+
+#Question 5
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+# Question 6
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+#Question 7
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+
+#Question 8
+for i in range(1,11):
+    for j in range(1,6):
+        print(j*i,end="\t")
+    print()
+
+# Question 9
+for i in range(1,4):
+    for j in range(1,6):
+        print(i*j,end=" ")
+    print()
+
+# Question 10
+for i in range(1,6):
+    for j in range(1,6):
+        print(j*j,end=" ")
+    print()
+
+#question 11
+for i in range(1,6):
+    for j in range(i):
+        print(chr(65+j),end=" ")
+    print()
+
+#Question 12
+for i in range(6):
+    for j in range(i):
+        print(chr(64+i),end=" ")
+    print()
+
+#question 13 
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(2*j-1,end=" ")
+    print()
+
+#question 14
+
